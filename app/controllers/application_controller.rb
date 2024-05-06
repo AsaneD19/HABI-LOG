65e702101d196477a4fb60214095846c9b82e416
AsaneD19 <159635329+AsaneD19@users.noreply.github.com>
2024-05-06 13:35:51 +0000

    アカウントネームでログイン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:account_name, :nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:account_name, :email, :nickname])
   end
 end
