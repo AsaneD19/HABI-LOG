bc47840c8e07b79c7dc49f1e37713c00b0ca5ef0
AsaneD19 <159635329+AsaneD19@users.noreply.github.com>
2024-05-14 21:49:08 +0000

    投稿へのコメント機能

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -5,20 +5,28 @@ class PostCommentsController < ApplicationController
   end
   def create
     @post_comment = PostComment.new(post_comment_params)
-    @feed = Feed.find(params[feed_id])
+    @feed = Feed.find(params[:feed_id])
     @post_comment.member_id = current_member.id
     @post_comment.target_feed_id = @feed.id
+
     if @post_comment.save
       flash[:notice] = "Your comment to feed has succeeded."
-      redirect_to feed_path(@feed.id)
+      redirect_to feed_path(params[:feed_id])
     else
-      flash[:alert] = @habit.errors.full_messages.join(", ")
+      flash[:alert] = @post_comment.errors.full_messages.join(", ")
+      @post_comments = @feed.post_comments
       render "feeds/show"
     end
 
   end
   def index
 
+  end
+  def destroy
+    post_comment = PostComment.find(params[:id])
+    post_comment.destroy
+    flash[:notice] = "Your comment has been deleted successfully."
+    redirect_to feed_path(params[:feed_id])
   end
 
   private
--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -1,8 +1,9 @@
 class PostComment < ApplicationRecord
     belongs_to :member
     belongs_to :target_feed, class_name: "Feed", foreign_key: "target_feed_id"
-    belongs_to :parent_post_comment, class_name: "PostComment", foreign_key: "target_post_comment_id"
+    # belongs_to :parent_post_comment, class_name: "PostComment", foreign_key: "target_post_comment_id"
 
-    has_many :replies, class_name: "PostComment", foreign_key: "target_post_comment_id", dependent: :destroy
+    # has_many :replies, class_name: "PostComment", foreign_key: "target_post_comment_id", dependent: :destroy
 
+    validates :comment, presence: true
 end
