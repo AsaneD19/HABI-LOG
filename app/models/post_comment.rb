18d3839c51558fad5e09a2ecf82a1beb69b066ff
AsaneD19 <159635329+AsaneD19@users.noreply.github.com>
2024-05-17 00:04:56 +0000

    on going

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -2,8 +2,5 @@ class PostComment < ApplicationRecord
     belongs_to :member
     belongs_to :target_feed, class_name: "Feed", foreign_key: "target_feed_id"
 
-    belongs_to :parent_post_comment, class_name: "PostComment", foreign_key: "target_post_comment_id"
-    has_many :replies, class_name: "PostComment", foreign_key: "target_post_comment_id", dependent: :destroy
-
     validates :content, presence: true
 end
