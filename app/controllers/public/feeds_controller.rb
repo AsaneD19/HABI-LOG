fd65c7478d92df732eb58f157c3ae7d5485148ad
AsaneD19 <159635329+AsaneD19@users.noreply.github.com>
2024-07-21 17:21:14 +0000

    feedレコード取得処理変更

--- app/controllers/public/feeds_controller.rb
+++ app/controllers/public/feeds_controller.rb
@@ -4,7 +4,8 @@ class Public::FeedsController < ApplicationController
   before_action ->{is_matching_login_member(Feed.find(params[:id]).member)}, only: [:destroy]
 
   def index
-    @feeds = Feed.joins(:member).where(members: { is_active: true }).where(members: { is_private: false }).order(created_at: :desc)
+    @feeds = Feed.all.order(created_at: :desc)
+
   end
 
   def show
