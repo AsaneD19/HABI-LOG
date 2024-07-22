0dead4d20658a0d45cbb5d57548791614f983740
AsaneD19 <159635329+AsaneD19@users.noreply.github.com>
2024-07-22 19:25:09 +0000

    GitHub Actions：初期データ投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,23 @@ Tag.delete_all
 Tag.create!(
   [
     {
+      id: 1,
       name: '勉強'
     },
     {
+      id: 2,
       name: '仕事'
     },
     {
+      id: 3,
       name: '生活'
     },
     {
+      id: 4,
       name: '趣味'
     },
     {
+      id: 5,
       name: '思考'
     }
   ]
